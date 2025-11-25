b15eeca3b61a57fa98ae67c030a25269f0109490
Person-0 <atulyajain28122007@gmail.com>
2025-11-25 18:25:12 +0530

    added click logo drop thing

--- assets/index.js
+++ assets/index.js
@@ -8,20 +8,30 @@ function resize_canvas() {
 resize_canvas();
 window.onresize = resize_canvas();
 
+const GRAVITY = 1.5;
 const MAX_TRAILS = 10;
+const gravitylerpfac = 0.00045;
+
 const cursor = {
     x: -Infinity,
     y: -Infinity,
     r: 50
 }
+const androidLogo = new Image();
+androidLogo.src = "./assets/icon.png";
 
 let lastRecords = [];
+let clickRecords = [];
 
 window.addEventListener("mousemove", ({ clientX, clientY }) => {
     cursor.x = clientX;
     cursor.y = clientY;
 
     lastRecords.push({x: cursor.x, y: cursor.y});    
+})
+
+window.addEventListener("click", () => {
+    clickRecords.push({...cursor, velocity: 0});
 })
 
 function drawCircle(x, y) {
@@ -40,18 +50,32 @@ function removeLastCursorInstance() {
     }
 }
 
-let lastTime = performance.now();
+let lastRemoveTime = performance.now();
+let lastFrameTime = 0;
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const now = performance.now();
+    const delta = Math.min(now - lastFrameTime, 100);
 
     for(const record of lastRecords) {
         drawCircle(record.x, record.y);
     }
 
-    const now = performance.now();
-    if(now - lastTime >= 7) {
+    for(const clickRecord of clickRecords){
+        if(clickRecord.y > window.innerHeight){
+            clickRecords = clickRecords.filter(e => e.x !== clickRecord.x && e.y !== clickRecord.y);
+            continue;
+        }
+        ctx.drawImage(androidLogo, clickRecord.x, clickRecord.y, 25, 25);
+
+        clickRecord.velocity = clickRecord.velocity + (clickRecord.velocity + GRAVITY) * (delta * gravitylerpfac);
+        clickRecord.y += clickRecord.velocity;
+    }
+
+    if(now - lastRemoveTime >= 7) {
         removeLastCursorInstance();
-        lastTime = now;
+        lastRemoveTime = now;
     }
     
     requestAnimationFrame(animate);
