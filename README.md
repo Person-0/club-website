e6eb9b9da79a9da40ad3e8cf746ab49f244e4ba0
Person-0 <atulyajain28122007@gmail.com>
2025-11-25 17:57:02 +0530

    updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a sample website I made as a task to join a well-reputed club in my university.
 
 Text content is AI Generated to speed up the process of making this website.
+
+Visit the website hosted by [clicking here](https://person-0.github.io/club-website/)
---
+++ assets/index.js
@@ -0,0 +1,60 @@
+const canvas = document.getElementById("cursor");
+const ctx = canvas.getContext("2d");
+
+function resize_canvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize_canvas();
+window.onresize = resize_canvas();
+
+const cursor = {
+    x: -Infinity,
+    y: -Infinity,
+    r: 50
+}
+
+let lastRecords = [];
+
+canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
+    cursor.x = clientX;
+    cursor.y = clientY;
+
+    lastRecords.push({x: cursor.x, y: cursor.y});    
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.arc(x, y, cursor.r, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+function removeLastCursorInstance() {
+    const len = lastRecords.length;
+    if(len > 0) {
+        if(len > 20) {
+            lastRecords = lastRecords.slice(20, len);
+            return;
+        }
+        lastRecords.shift();
+    }
+}
+
+let lastTime = performance.now();
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(const record of lastRecords) {
+        drawCircle(record.x, record.y);
+    }
+
+    const now = performance.now();
+    if(now - lastTime >= 7) {
+        removeLastCursorInstance();
+        lastTime = now;
+    }
+    
+    requestAnimationFrame(animate);
+}
+animate();
